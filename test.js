2a91a25ad549a626daff913bdfcc06bdcb4e7d40
Rabadan Bakhmudov <brabadan@yandex.ru>
2020-07-28 22:43:14 +0300

    pyatnaski: available move line

--- test.js
+++ test.js
@@ -1,34 +1,48 @@
-var Fishka = (function () {
+var Fishka = /** @class */ (function () {
     function Fishka(num, posX, posY, pyatnashki) {
         var _this = this;
         this.num = num;
         this.posX = posX;
         this.posY = posY;
         this.pyatnashki = pyatnashki;
+        this.draggable = false;
         this.el = document.createElement('div');
         this.el.classList.add('fishka');
         this.el.innerHTML = '<span style="font-size: ' + (pyatnashki.fishkaWidth + pyatnashki.fishkaHeight) / 4 + '">' + this.num + '</span>';
-        this.el.onmousedown = function (ev) { _this.onclick(ev); return false; };
+        this.el.onmousedown = function (ev) {
+            _this.onclick(ev);
+            return false;
+        };
         this.show();
         pyatnashki.parentEl.appendChild(this.el);
     }
     Fishka.prototype.onclick = function (ev) {
+        var _a, _b;
+        if (this.draggable) {
+            this.pyatnashki.onClickDraggable(this.posX, this.posY);
+        }
+        return;
         if ((this.posX == this.pyatnashki.pustoX && (this.posY == this.pyatnashki.pustoY + 1 || this.posY == this.pyatnashki.pustoY - 1)) || (this.posY == this.pyatnashki.pustoY && (this.posX == this.pyatnashki.pustoX + 1 || this.posX == this.pyatnashki.pustoX - 1))) {
             _a = [this.pyatnashki.pustoX, this.posX], this.posX = _a[0], this.pyatnashki.pustoX = _a[1];
             _b = [this.pyatnashki.pustoY, this.posY], this.posY = _b[0], this.pyatnashki.pustoY = _b[1];
             this.show();
             this.pyatnashki.checkWin();
         }
-        var _a, _b;
     };
     ;
     Fishka.prototype.show = function () {
         var _a = [this.pyatnashki.fishkaWidth, this.pyatnashki.fishkaHeight], width = _a[0], height = _a[1];
         this.el.setAttribute('style', 'left: ' + this.posX * width + 'px; top: ' + this.posY * height + 'px; width: ' + (width - 6) + 'px; height: ' + (height - 6) + 'px');
+        if (this.draggable) {
+            this.el.classList.add('draggable');
+        }
+        else {
+            this.el.classList.remove('draggable');
+        }
     };
     return Fishka;
 }());
-var Pyatnashki = (function () {
+var Pyatnashki = /** @class */ (function () {
     function Pyatnashki(id, colX, colY) {
         if (colX === void 0) { colX = 4; }
         if (colY === void 0) { colY = 4; }
@@ -41,7 +55,9 @@ var Pyatnashki = (function () {
         this.fishkaWidth = this.parentEl.clientWidth / colX;
         this.fishkaHeight = this.parentEl.clientHeight / colY;
         this.initFishki();
-        this.parentEl.onmousedown = this.parentEl.onselectstart = function () { return false; };
+        this.parentEl.onmousedown = this.parentEl.onselectstart = function () {
+            return false;
+        };
     }
     Pyatnashki.prototype.initFishki = function () {
         for (var y = 0; y < this.colY; y++) {
@@ -53,14 +69,51 @@ var Pyatnashki = (function () {
         }
         this.fishki[0].el.parentNode.removeChild(this.fishki[0].el);
         this.fishki = this.fishki.slice(1);
-        this.fishki.forEach(function (fishka) { fishka.show(); });
+        this.initDraggables();
+    };
+    Pyatnashki.prototype.initDraggables = function () {
+        var _this = this;
+        this.fishki.forEach(function (fishka) {
+            fishka.draggable = fishka.posX === _this.pustoX || fishka.posY === _this.pustoY;
+            fishka.show();
+        });
+    };
+    Pyatnashki.prototype.onClickDraggable = function (posX, posY) {
+        var _a;
+        var _this = this;
+        if (posX !== this.pustoX && posY !== this.pustoY) {
+            return;
+        }
+        if (posX === this.pustoX) {
+            var delta_1 = Math.abs(this.pustoY - posY) / (this.pustoY - posY);
+            this.fishki.forEach(function (fishka) {
+                if ((fishka.posX === _this.pustoX &&
+                    (_this.pustoY < fishka.posY && fishka.posY <= posY || _this.pustoY > fishka.posY && fishka.posY >= posY))) {
+                    fishka.posY += delta_1;
+                }
+            });
+        }
+        if (posY === this.pustoY) {
+            var delta_2 = Math.abs(this.pustoX - posX) / (this.pustoX - posX);
+            this.fishki.forEach(function (fishka) {
+                if ((fishka.posY === _this.pustoY &&
+                    (_this.pustoX < fishka.posX && fishka.posX <= posX || _this.pustoX > fishka.posX && fishka.posX >= posX))) {
+                    fishka.posX += delta_2;
+                }
+            });
+        }
+        _a = [posX, posY], this.pustoX = _a[0], this.pustoY = _a[1];
+        this.initDraggables();
+        this.checkWin();
     };
     Pyatnashki.prototype.checkWin = function () {
         for (var i = 0; i < this.fishki.length; i++) {
             if (this.fishki[i].num != this.fishki[i].posX + this.fishki[i].posY * this.colX + 1)
                 return;
         }
-        alert('!!!!!! Поздравляю, Вы собрали головоломку !!!!!!!!!!!');
+        setTimeout(function () {
+            alert('!!!!!! Поздравляю, Вы собрали головоломку !!!!!!!!!!!');
+        }, 200);
     };
     return Pyatnashki;
 }());
--- test.ts
+++ test.ts
@@ -1,18 +1,27 @@
 class Fishka {
     public el: HTMLElement;
+    public draggable = false;
 
-    constructor (public num, public posX: number, public posY: number, public pyatnashki: Pyatnashki) {
+    constructor(public num, public posX: number, public posY: number, public pyatnashki: Pyatnashki) {
         this.el = document.createElement('div');
         this.el.classList.add('fishka');
-        this.el.innerHTML = '<span style="font-size: ' + (pyatnashki.fishkaWidth + pyatnashki.fishkaHeight)/4 + '">' + this.num + '</span>';
-        this.el.onmousedown = (ev)=>{this.onclick(ev); return false;};
+        this.el.innerHTML = '<span style="font-size: ' + (pyatnashki.fishkaWidth + pyatnashki.fishkaHeight) / 4 + '">' + this.num + '</span>';
+        this.el.onmousedown = (ev) => {
+            this.onclick(ev);
+            return false;
+        };
         this.show();
 
         pyatnashki.parentEl.appendChild(this.el);
     }
 
     onclick(ev) {
-        if ( (this.posX == this.pyatnashki.pustoX && (this.posY == this.pyatnashki.pustoY + 1 || this.posY == this.pyatnashki.pustoY - 1)) || (this.posY == this.pyatnashki.pustoY && (this.posX == this.pyatnashki.pustoX + 1 || this.posX == this.pyatnashki.pustoX - 1)) ) {
+        if (this.draggable) {
+            this.pyatnashki.onClickDraggable(this.posX, this.posY);
+        }
+        return;
+        
+        if ((this.posX == this.pyatnashki.pustoX && (this.posY == this.pyatnashki.pustoY + 1 || this.posY == this.pyatnashki.pustoY - 1)) || (this.posY == this.pyatnashki.pustoY && (this.posX == this.pyatnashki.pustoX + 1 || this.posX == this.pyatnashki.pustoX - 1))) {
             [this.posX, this.pyatnashki.pustoX] = [this.pyatnashki.pustoX, this.posX];
             [this.posY, this.pyatnashki.pustoY] = [this.pyatnashki.pustoY, this.posY];
             this.show();
@@ -22,12 +31,17 @@ class Fishka {
 
     show() {
         let [width, height] = [this.pyatnashki.fishkaWidth, this.pyatnashki.fishkaHeight];
-        this.el.setAttribute('style', 'left: ' + this.posX * width + 'px; top: ' + this.posY * height + 'px; width: ' + (width-6) + 'px; height: ' + (height-6) + 'px');
+        this.el.setAttribute('style', 'left: ' + this.posX * width + 'px; top: ' + this.posY * height + 'px; width: ' + (width - 6) + 'px; height: ' + (height - 6) + 'px');
+        if (this.draggable) {
+            this.el.classList.add('draggable');
+        } else {
+            this.el.classList.remove('draggable');
+        }
     }
 }
 
 class Pyatnashki {
-    public fishki : Fishka[] = [];
+    public fishki: Fishka[] = [];
     public parentEl: HTMLElement;
     public fishkaWidth: number;
     public fishkaHeight: number;
@@ -39,27 +53,70 @@ class Pyatnashki {
         this.fishkaWidth = this.parentEl.clientWidth / colX;
         this.fishkaHeight = this.parentEl.clientHeight / colY;
         this.initFishki();
-        this.parentEl.onmousedown = this.parentEl.onselectstart = function() {return false};
+        this.parentEl.onmousedown = this.parentEl.onselectstart = function () {
+            return false
+        };
     }
 
     initFishki() {
-        for (let y=0; y<this.colY; y++) {
-            for (let x=0; x<this.colX; x++) {
-                let num = this.colX*this.colY - x - this.colX*y;
+        for (let y = 0; y < this.colY; y++) {
+            for (let x = 0; x < this.colX; x++) {
+                let num = this.colX * this.colY - x - this.colX * y;
                 let fishka = new Fishka(num, x, y, this);
                 this.fishki.push(fishka);
             }
         }
         this.fishki[0].el.parentNode.removeChild(this.fishki[0].el);
         this.fishki = this.fishki.slice(1);
-        this.fishki.forEach((fishka)=> {fishka.show()});
+        this.initDraggables();
+    }
+
+    initDraggables() {
+        this.fishki.forEach(fishka => {
+            fishka.draggable = fishka.posX === this.pustoX || fishka.posY === this.pustoY;
+            fishka.show();
+        })
+    }
+    
+    onClickDraggable(posX: number, posY: number) {
+        if (posX !== this.pustoX && posY !== this.pustoY) {
+            return;
+        }
+        if (posX === this.pustoX) {
+            const delta = Math.abs(this.pustoY - posY) / (this.pustoY - posY);
+            this.fishki.forEach(fishka => {
+                if (
+                    (fishka.posX === this.pustoX && 
+                    (this.pustoY < fishka.posY && fishka.posY <= posY || this.pustoY > fishka.posY && fishka.posY >= posY))
+                ) {
+                    fishka.posY += delta;
+                }
+            })
+        } 
+        if (posY === this.pustoY) {
+            const delta = Math.abs(this.pustoX - posX) / (this.pustoX - posX);
+            this.fishki.forEach(fishka => {
+                if (
+                    (fishka.posY === this.pustoY &&
+                        (this.pustoX < fishka.posX && fishka.posX <= posX || this.pustoX > fishka.posX && fishka.posX >= posX))
+                ) {
+                    fishka.posX += delta;
+                }
+            })
+        }
+        [this.pustoX, this.pustoY] = [posX, posY];
+        
+        this.initDraggables();
+        this.checkWin();
     }
 
     checkWin() {
-        for (let i=0; i<this.fishki.length; i++) {
-            if (this.fishki[i].num != this.fishki[i].posX + this.fishki[i].posY*this.colX +1) return;
+        for (let i = 0; i < this.fishki.length; i++) {
+            if (this.fishki[i].num != this.fishki[i].posX + this.fishki[i].posY * this.colX + 1) return;
         }
-        alert('!!!!!! Поздравляю, Вы собрали головоломку !!!!!!!!!!!');
+        setTimeout(() => {
+            alert('!!!!!! Поздравляю, Вы собрали головоломку !!!!!!!!!!!');
+        }, 200);
     }
 
 }
